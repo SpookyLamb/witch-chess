584768373f7b0d617381d97b14711f052067185d
SpookyLamb <turtleytyrant@gmail.com>
2024-06-12 09:38:04 -0400

    Authentication.

--- src/api.js
+++ src/api.js
@@ -4,18 +4,18 @@ export const baseUrl = 'http://127.0.0.1:8000' //local dev
 
 //helper functions
 
-export function saveLogin(authToken, authRefresh, username) {
+export function saveLogin(authToken, authRefresh) {
     localStorage.setItem("access", authToken)
     localStorage.setItem("refresh", authRefresh)
-    localStorage.setItem("username", username)
+    //localStorage.setItem("username", username)
     console.log("Saved login information.")
 }
 
 export function deleteLogin() {
     localStorage.removeItem("access")
     localStorage.removeItem("refresh")
-    localStorage.removeItem("username")
-    localStorage.removeItem("userID")
+    //localStorage.removeItem("username")
+    //localStorage.removeItem("userID")
     console.log("Deleted old login information.")
 }
 
@@ -41,25 +41,24 @@ export const getToken = ({ auth, username, password }) => {
         password,
     }).then(response => {
         auth.setAccessToken(response.data.access)
-        auth.setUsername(username)
-        getUserID(response.data.access, auth)
-        saveLogin(response.data.access, response.data.refresh, username)
+        // getUserID(response.data.access, auth)
+        saveLogin(response.data.access, response.data.refresh)
     }).catch(error => console.log("ERROR: ", error))
 }
 
-const getUserID = (accessToken, auth) => {
-    axios({
-        method: 'get',
-        url: `${baseUrl}/user-id/`,
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        },
-    }).then(response => {
-        console.log("GET USER RESPONSE: ", response)
-        auth.setUserID(response.data.id)
-        localStorage.setItem("userID", response.data.id)
-    }).catch(error => console.log("ERROR: ", error))
-}
+// const getUserID = (accessToken, auth) => {
+//     axios({
+//         method: 'get',
+//         url: `${baseUrl}/user-id/`,
+//         headers: {
+//             Authorization: `Bearer ${accessToken}`
+//         },
+//     }).then(response => {
+//         console.log("GET USER RESPONSE: ", response)
+//         auth.setUserID(response.data.id)
+//         localStorage.setItem("userID", response.data.id)
+//     }).catch(error => console.log("ERROR: ", error))
+// }
 
 export const fetchUser = ({ auth }) => {
     axios({
