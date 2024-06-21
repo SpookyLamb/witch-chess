445307d84e62cfb1fec8b725113b028b791ef07e
SpookyLamb <turtleytyrant@gmail.com>
2024-06-21 09:37:26 -0400

    Improved rules page.

--- src/Game.jsx
+++ src/Game.jsx
@@ -43,6 +43,15 @@ const imageSources = {
     whiteRook: `${imgUrl}/white-rook.png`,
     whiteKnight: `${imgUrl}/white-knight.png`,
     whiteBishop: `${imgUrl}/white-bishop.png`,
+
+    whiteSmite: `${imgUrl}/white-smite.png`,
+    blackSmite: `${imgUrl}/black-smite.png`,
+    whiteTime: `${imgUrl}/white-time-stop.png`,
+    blackTime: `${imgUrl}/black-time-stop.png`,
+    whiteRaise: `${imgUrl}/white-raise-dead.png`,
+    blackRaise: `${imgUrl}/black-raise-dead.png`,
+    whiteTele: `${imgUrl}/white-telekinesis.png`,
+    blackTele: `${imgUrl}/black-telekinesis.png`,
 }
 
 function LobbyInput(props) {
@@ -211,7 +220,7 @@ function Lobby(props) {
 
 function Title() {
     return (
-        <h1 className="text-white text-center">
+        <h1 className="text-white text-center pt-3">
             WITCH CHESS
         </h1>
     )
@@ -237,9 +246,83 @@ function RulesButton(props) {
 
 function Rules(props) {
     const setRulesVisible = props.setRulesVisible
+    const pages = {
+        1: (<>
+        <h3>Witch Chess Rules (1/2)</h3>
+        <p>Anyone totally unfamiliar with the basic rules of Chess should refer to 
+            <a href="https://en.wikipedia.org/wiki/Rules_of_chess" target="_blank"> this Wikipedia article</a> for the full rules.
+        </p>
+        <p>
+            In brief: Chess is a turn-based strategy game where your goal is to capture the enemy's King. In Witch Chess, the pieces are the following:
+        </p>
+        <ul>
+        <li><img className="rules-piece" src={imageSources.whitePawn}/> <img className="rules-piece" src={imageSources.blackPawn}/> &nbsp;Pawns </li>
+        <li><img className="rules-piece" src={imageSources.whiteKnight}/> <img className="rules-piece" src={imageSources.blackKnight}/> &nbsp;Knights </li>
+        <li><img className="rules-piece" src={imageSources.whiteBishop}/> <img className="rules-piece" src={imageSources.blackBishop}/> &nbsp;Bishops </li>
+        <li><img className="rules-piece" src={imageSources.whiteRook}/> <img className="rules-piece" src={imageSources.blackRook}/> &nbsp;Rooks </li>
+        <li><img className="rules-piece" src={imageSources.whiteQueen}/> <img className="rules-piece" src={imageSources.blackQueen}/> &nbsp;Queens </li>
+        <li><img className="rules-piece" src={imageSources.whiteKing}/> <img className="rules-piece" src={imageSources.blackKing}/> &nbsp;Kings </li>
+        </ul>
+        <p>
+            <strong>Witches are impatient!</strong> You only have three minutes for each game (gaining two seconds when you make a move). 
+            Running out of time is an instant loss! It's better to make a bad move quickly than spend too much time trying to 
+            come up with the perfect move!
+        </p>
+        <p>
+            <strong>Witches are competitive!</strong> One game isn't enough for them to crown a victor. The first witch to win three games 
+            (or the one with the most wins in five games) is considered the true winner. 
+        </p>
+        </>),
+        2: (<>
+        <h3>Witch Chess Rules (2/2)</h3>
+        <p>
+            <strong>Witches cast spells!</strong> Unlike normal chess, in Witch Chess, you can use once-per-game spells that can influence the board 
+            and give an edge over your opponent (ideally before they do the same to you). The spells are as follows:
+        </p>
+        <ul>
+        <li><img className="rules-piece" src={imageSources.whiteSmite}/><img className="rules-piece" src={imageSources.blackSmite}/> &nbsp; <strong>Smite</strong>
+        <p>Use your turn to instantly capture an enemy piece threatened by one of your pieces, without moving a piece. Cannot target the King or Queen.</p>
+        <p>TO USE: Click the spell, then click a valid piece, turning it to ash.</p>
+        </li>
+        <li><img className="rules-piece" src={imageSources.whiteTime}/><img className="rules-piece" src={imageSources.blackTime}/> &nbsp; <strong>Time Stop</strong>
+        <p>Activate, and then skip your opponent's next turn, so long as neither King is in Check at the beginning or end of your first turn.</p>
+        <p>TO USE: Click the spell, and then take your turn. Putting a King in Check cancels the spell.</p>
+        </li>
+        <li><img className="rules-piece" src={imageSources.whiteRaise}/><img className="rules-piece" src={imageSources.blackRaise}/> &nbsp; <strong>Raise Dead</strong>
+        <p>Use your turn to return one of your captured pieces to a valid starting position (cannot be used on a piece if their position(s) is/are occupied).</p>
+        <p>TO USE: Click the spell, click one of your captured pieces, and then click a valid (unoccupied) starting square.</p>
+        </li>
+        <li><img className="rules-piece" src={imageSources.whiteTele}/><img className="rules-piece" src={imageSources.blackTele}/> &nbsp; <strong>Telekinesis</strong>
+        <p>Instead of moving one of your own pieces, use your turn to move an opponent's pawn, following normal movement rules.</p>
+        <p>TO USE: Click the spell, and then move an enemy pawn.</p>
+        </li>
+        </ul>
+        <p><strong>To cancel a spell</strong>, simply click the same spell again.</p>
+        </>),
+    }
+    const [pageNum, setPageNum] = useState(1)
 
     function noRules() {
         setRulesVisible(false)
+    }
+
+    function changePage(increment) {
+        if (increment) {
+            if (pageNum + 1 < 3) {
+                setPageNum(pageNum + 1)
+            }
+        } else {
+            if (pageNum - 1 > 0) {
+                setPageNum(pageNum - 1)
+            }
+        }
+    }
+
+    let pageButt
+    if (pageNum === 1) {
+        pageButt = (<Button variant="contained" size="large" className="mybutton mx-1" onClick={() => changePage(true)}>Next</Button>)
+    } else {
+        pageButt = (<Button variant="contained" size="large" className="mybutton mx-1" onClick={() => changePage(false)}>Back</Button>)
     }
 
     return (
@@ -248,36 +331,13 @@ function Rules(props) {
                 <Container>
                     <Row>
                         <Col>
-                        <h3>Witch Chess Rules</h3>
-                        <p>Anyone totally unfamiliar with the basic rules of Chess should refer to 
-                            <a href="https://en.wikipedia.org/wiki/Rules_of_chess" target="_blank"> this Wikipedia article</a> for the full rules.
-                        </p>
-                        <p>
-                            In brief: Chess is a turn-based strategy game where your goal is to capture the enemy's King. In Witch Chess, the pieces are the following:
-                        </p>
-                        <ul>
-                        <li>Pawns&nbsp;&nbsp; <img className="rules-piece" src={imageSources.whitePawn}/> <img className="rules-piece" src={imageSources.blackPawn}/></li>
-                        <li>Knights <img className="rules-piece" src={imageSources.whiteKnight}/> <img className="rules-piece" src={imageSources.blackKnight}/></li>
-                        <li>Bishops <img className="rules-piece" src={imageSources.whiteBishop}/> <img className="rules-piece" src={imageSources.blackBishop}/></li>
-                        <li>Rooks&nbsp;&nbsp; <img className="rules-piece" src={imageSources.whiteRook}/> <img className="rules-piece" src={imageSources.blackRook}/></li>
-                        <li>Queens&nbsp; <img className="rules-piece" src={imageSources.whiteQueen}/> <img className="rules-piece" src={imageSources.blackQueen}/></li>
-                        <li>Kings&nbsp;&nbsp; <img className="rules-piece" src={imageSources.whiteKing}/> <img className="rules-piece" src={imageSources.blackKing}/></li>
-                        </ul>
-                        <p>
-                            <strong>Witches are impatient!</strong> You only have three minutes for each game (gaining two seconds when you make a move), 
-                            and running out of time is an instant loss! It's better to make a bad move quickly than spend too much time trying to 
-                            come up with the perfect move!
-                        </p>
-                        <p>
-                            <strong>Witches are competitive!</strong> One game isn't enough for them to crown a victor. The first witch to win three games 
-                            (or the one with the most wins in five games) is considered the true winner. 
-                        </p>
-
+                            {pages[pageNum]}
                         </Col>
                     </Row>
                     <Row>
                         <Col className="pt-2 text-center">
-                            <Button variant="contained" size="large" className="mybutton" onClick={() => noRules()}>BACK</Button>
+                            <Button variant="contained" size="large" className="mybutton mx-1" onClick={() => noRules()}>OK</Button>
+                            {pageButt}
                         </Col>
                     </Row>
                 </Container>
